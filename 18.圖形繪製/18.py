6208b0dd761077f1253db706631dac69982f6c80
06wuuntt <wuuntt21@gmail.com>
2024-10-28 21:09:33 +0800

    Add 18.py

--- 18.圖形繪製/18.py
+++ 18.圖形繪製/18.py
@@ -1,29 +1,49 @@
-def drawMark(mark,n):
+def drawMark(mark, n):
     for i in range(n):
-        print(mark,end='')
+        print(mark, end='')
 
 def drawTriangle(n):
-    for i in range(1,n+1):
-        drawMark('#',n-i)
-        drawMark('*',2*i-1)
-        drawMark('#',n-i)
+    for i in range(1, n+1):
+        drawMark('#', n-i)
+        drawMark('*', 2*i-1)
+        drawMark('#', n-i)
+        print('')
+
+def drawInvertedTriangle(n):
+    for i in range(n, 0, -1):
+        drawMark('#', n-i)
+        drawMark('*', 2*i-1)
+        drawMark('#', n-i)
         print('')
 
 def drawRhombus(n):
-    for i in range(n):
-        drawMark()
-
-def drawInvertedTriangle(n):
-    for i in range(n,0,-1):
-        drawMark('#',n-i)
-        drawMark('*',2*i-1)
-        drawMark('#',n-i)
+    for i in range(n//2, -1, -1):
+        drawMark(' ', i)
+        drawMark('*', 2*((n//2)-i)+1)
+        print('')
+        
+    for i in range(1,(n//2)+1):
+        drawMark(' ', i)
+        drawMark('*', 2*((n//2)-i)+1)
         print('')
 
+def drawFish(n):
+    for i in range(n//2,-1,-1):
+        drawMark(' ', i)
+        drawMark('*', 2*((n//2)-i)+1)
+        drawMark(' ', 2*(i+1)-2)
+        drawMark('-', (n//2)-i)
+        print('')
 
+    for i in range(1,(n//2)+1):
+        drawMark(' ', i)
+        drawMark('*', 2*((n//2)-i)+1)
+        drawMark(' ', 2*i)
+        drawMark('-', (n//2)-i)
+        print('')
 
-def main():
-    c,n = int(input()),int(input())
+def main(): 
+    c, n = int(input()), int(input())
     if n <= 2 or n >= 50 or n%2 == 0:
         print('error')
     else:
@@ -33,5 +53,7 @@ def main():
             drawInvertedTriangle(n)
         elif c == 3:
             drawRhombus(n)
+        elif c == 4:
+            drawFish(n)
 
 main()
